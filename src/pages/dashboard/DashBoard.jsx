ecb4662f71ff13115cd59899ea72a522fb558bf2
Tripleoniks <84743221+Tripleoniks@users.noreply.github.com>
2022-09-19 11:19:23 +0100

    udux streaming platform

--- src/pages/dashboard/DashBoard.jsx
+++ src/pages/dashboard/DashBoard.jsx
@@ -1,26 +1,37 @@
-import React, { useEffect, useState } from "react";
-import NavBar from "../../components/navbar";
-import Row from "../../components/row";
-import Banner from "../../components/banner";
-import SideBar from "../../components/sidebar";
-import { Container, DashBoardContainer, Wrapper } from "./style";
-import { songs } from "../../data/songs";
+import React, { useEffect, useState } from 'react';
+import NavBar from '../../components/navbar';
+import Row from '../../components/row';
+import Banner from '../../components/banner';
+import SideBar from '../../components/sidebar';
+import { Container, DashBoardContainer, Wrapper } from './style';
+import { songs } from '../../data/songs';
 
 const DashBoard = () => {
   const [items, setItems] = useState([]);
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
 
+  // This function is used to filter the cards based on the query- Title
   useEffect(() => {
     const fetchItems = async () => {
       // By title
       const result = songs.filter((value) =>
         value.title.toLowerCase().includes(query.toLocaleLowerCase())
       );
-      // By artists
-      // const result = songs.filter((value) =>
-      // value.artists.join().toLowerCase().includes(query);
-      // );
+      setItems(result);
+    };
 
+    fetchItems();
+  }, [query]);
+
+  // This function is used to filter the cards based on the query- Artist
+  useEffect(() => {
+    const fetchItems = async () => {
+      //  by artist
+      const result = songs.filter((value) =>
+        value.artists.some((artist) =>
+          artist.toLowerCase().includes(query.toLocaleLowerCase())
+        )
+      );
       setItems(result);
     };
 
@@ -38,8 +49,8 @@ const DashBoard = () => {
       <DashBoardContainer>
         <Container>
           <Banner />
-          <Row title={"Welcome Back"} songs={items} />
-          <Row title={"Cheers to the Weekend "} songs={items} />
+          <Row title={'Welcome Back'} songs={items} />
+          <Row title={'Cheers to the Weekend '} songs={items} />
         </Container>
       </DashBoardContainer>
     </Wrapper>
